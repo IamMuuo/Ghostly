8fcf2b20c63c05e6bbcff37ea750096f1df70f59
erick <hearteric57@gmail.com>
2022-08-14 18:06:46 +0300

    got enemy to move to and fro

--- Enemy.gd
+++ Enemy.gd
@@ -1,41 +1,39 @@
 extends Area2D
 
+export var speed = 100;
+export var moveDist = 100;
 
-export var speed: int = 100
-export var moveDist: int = 100
+onready var startX = position.x
+onready var targetX = position.x + moveDist
 
-onready var startX :float = position.x
-onready var targetX :float = position.x + moveDist
+func _physics_process(delta):
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	position.x = move_to(position.x, targetX, speed * delta)
 	
-	
-	if position.x  == targetX:
+	if position.x == targetX:
 		if targetX == startX:
-			targetX = position.x + moveDist
+			targetX = position.x  + moveDist
 		else:
-			targetX = position.x
+			targetX = startX
 
-func move_to(from, to, step):
-	var new = from
+func move_to (current, to, step):
+	
+	var new = current
 	
 	if new < to:
 		new += step
 		
 		if new > to:
 			new = to
-	
 	else:
 		new -= step
 		
 		if new < to:
 			new = to
-	
+			
 	return new
+
+func _on_Enemy_body_entered(body):
+	
+	if body.name == "Player": 
+		body.die()
--- Player.gd
+++ Player.gd
@@ -1,38 +1,46 @@
 extends KinematicBody2D
 
+var score = 0
 
-# game stats
-var score: int = 0
+const speed = 200
+const jumpForce = 600
+const gravity = 800
 
-# game physics
-var speed: int = 100
-var gravity: int = 800
-var jumpForce: int = 600
-
-var vel: Vector2 = Vector2()
-var grounded: bool = false
+var vel = Vector2()
+var grounded = false
 
 onready var sprite = $Sprite
+onready var ui = get_node("/root/MainScene/CanvasLayer/UI")
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	vel.x = 0
+# physics loop
+func _physics_process(delta):
 	
+	vel.x = 0	
+
+	# movement inputs
 	if Input.is_action_pressed("move_left"):
 		vel.x -= speed
 	if Input.is_action_pressed("move_right"):
 		vel.x += speed
 	
-	vel = move_and_slide(vel,Vector2.UP)
+	# applying the velocity
+	vel = move_and_slide(vel, Vector2.UP)
 	
-	vel.y = gravity * delta
-	
+	# gravity
+	vel.y += gravity * delta
+		
+	# jump inputs
 	if Input.is_action_pressed("jump") and is_on_floor():
 		vel.y -= jumpForce
-		
+	
+	
+
+# called when we run into a coin
+func collect_coin (value):
+	
+	score += value
+	ui.set_score_text(score)
+
+func die ():
+	
+	get_tree().reload_current_scene()
