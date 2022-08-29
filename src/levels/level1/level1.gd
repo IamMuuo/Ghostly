332169b372d4418a0f43f109f6b0d5d789ca95e9
erick <hearteric57@gmail.com>
2022-08-29 19:54:24 +0300

    Completed refactoring level 1

--- src/levels/level1/level1.gd
+++ src/levels/level1/level1.gd
@@ -2,13 +2,34 @@ extends Node2D
 
 onready var player = get_node("Player")
 onready var camera = get_node("camera")
+onready var ui = get_node("CanvasLayer/Container")
+onready var pause = get_node("PauseGui/PauseMenu")
+onready var music = get_node("levelMusic")
+
+var db = {} # A dictionary to store the data
+var game_file = File.new()
+
 
 func _ready():
 	camera.position = player.position
+	pause.visible = false
+	music.play()
 	
 func _physics_process(delta):
 	camera.position = player.position
+	ui.set_score_text(player.score, player.health)
+	db["player_position"] = player.position
 
 
 func _on_camera_ready():
 	camera.position = player.position
+
+
+func _on_Level1_exiting():
+	game_file.open("user://save_game.dat", File.WRITE)
+	db["player_health"] = player.health
+	db["player_score"] = player.score
+	print(db)
+	game_file.store_var(db)
+	game_file.close()
+	
