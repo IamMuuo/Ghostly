b3b02ea14aad51cbbe79a64dbaf2c2e15dadce38
erick <hearteric57@gmail.com>
2022-08-31 17:50:42 +0300

    Added level 4 backgrounnd music

--- src/levels/level4/Level4.gd
+++ src/levels/level4/Level4.gd
@@ -4,11 +4,13 @@ onready var camera = get_node("camera")
 onready var player = get_node("Player")
 onready var ui = get_node("CanvasLayer/Container")
 onready var pause = get_node("PauseGui/PauseMenu")
+onready var music = get_node("levelMusic")
 
 
 func _ready():
 	camera.position = player.position
 	pause.visible = false
+	music.play()
 	
 func _physics_process(delta):
 	camera.position = player.position
