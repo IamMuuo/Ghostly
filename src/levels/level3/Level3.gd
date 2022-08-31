987e0234aec4136dfc625531b2c01eed55764c6f
erick <hearteric57@gmail.com>
2022-08-31 18:11:28 +0300

    Got level 3 portal working

--- src/levels/level3/Level3.gd
+++ src/levels/level3/Level3.gd
@@ -3,8 +3,10 @@ extends Node2D
 onready var camera = get_node("camera")
 onready var player = get_node("Player")
 onready var ui = get_node("CanvasLayer/Container")
+onready var pause = get_node("PauseGui/PauseMenu")
 
 func _ready():
+	pause.visible = false
 	camera.position = player.position
 	
 func _physics_process(delta):
