8feb40b5d98b7c629dc06459c2d4d44393c45ba6
erick <hearteric57@gmail.com>
2022-09-02 16:49:07 +0300

    Added level 3 background music

--- src/levels/level3/Level3.gd
+++ src/levels/level3/Level3.gd
@@ -4,10 +4,12 @@ onready var camera = get_node("camera")
 onready var player = get_node("Player")
 onready var ui = get_node("CanvasLayer/Container")
 onready var pause = get_node("PauseGui/PauseMenu")
+onready var music = get_node("levelMusic")
 
 func _ready():
 	pause.visible = false
 	camera.position = player.position
+	music.play()
 	
 func _physics_process(delta):
 	camera.position = player.position
