a22af356a3186ff0fc486f7cad685a1a0a63d70d
erick <hearteric57@gmail.com>
2022-09-02 17:25:04 +0300

    Added level 5 music and redesigned pause menu

--- src/levels/level5/Level5.gd
+++ src/levels/level5/Level5.gd
@@ -3,6 +3,8 @@ extends Node2D
 onready var camera = get_node("camera")
 onready var player = get_node("Player")
 onready var ui = get_node("CanvasLayer/Container")
+onready var pause = get_node("PauseGui/PauseMenu")
+onready var music = get_node("levelMusic")
 
 func _physics_process(delta):
 	camera.position = player.position
@@ -11,3 +13,5 @@ func _physics_process(delta):
 func _ready():
 	camera.position = player.position
 	ui.set_score_text(player.score, player.health)
+	music.play()
+	pause.visible = false
